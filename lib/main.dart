6a61237ce5568d85065b86cf58bc510a2101b4ba
MeWe <firstapp8055@gmail.com>
2021-10-30 19:20:38 -0700

    Hemanth

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 //dhanush
+//Hemanth
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // This widget is the root of your appliflutter cation.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
