79d2342ad3e70475b1501ef2df478ce9ecb0923a
MeWe <firstapp8055@gmail.com>
2021-10-30 22:22:25 -0700

    Hemanth

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:ecommerce/splash.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 //dhanush
 //Hemanth
 void main() {
